3fd0bc6bd83215328c95744d883b44aa9887e6ef
rushikesh.thak <rushikesh.thak@vsplc.com>
2025-06-05 19:36:10 +0530

    First

--- app/src/main/java/com/marathi/demoappforgit/MainActivity.java
+++ app/src/main/java/com/marathi/demoappforgit/MainActivity.java
@@ -20,5 +20,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        //hello word
     }
 }
